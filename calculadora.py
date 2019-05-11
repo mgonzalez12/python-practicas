a804c1716a07c890218ce5d844f1677e078b1691
Marcos <eminence_03@hotmail.com>
2019-05-11 17:06:56 -0500

    Python-practica-uno

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def suma(num_uno,num_dos):
+    return num_uno + num_dos
+
+def resta(num_uno,num_dos):
+    return num_uno - num_dos
+
+def multiplicar(num_uno,num_dos):
+    return num_uno * num_dos
+
+def dividir(num_uno,num_dos):
+    return num_uno / num_dos    
---
+++ index.py
@@ -0,0 +1,4 @@
+import calculadora
+
+resultado = calculadora.suma(30,45)
+print(resultado)
